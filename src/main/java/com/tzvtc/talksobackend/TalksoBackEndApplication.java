bf1e7abba54c0471bf4c9fbaecec73333e7d8dd4
bent2685 <bent2685@outlook.com>
2023-02-24 20:55:26 +0800

    Initial commit

---
+++ src/main/java/com/tzvtc/talksobackend/TalksoBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.tzvtc.talksobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalksoBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalksoBackEndApplication.class, args);
+	}
+
+}
