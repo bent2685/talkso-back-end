1b7b763bcc7ab66a3b60ffc6101ed40fd81bc497
bent2685 <bent2685@outlook.com>
2023-02-28 20:43:47 +0800

    。

--- src/main/java/com/tzvtc/talksobackend/TalksoBackEndApplication.java
+++ src/main/java/com/tzvtc/talksobackend/TalksoBackEndApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TalksoBackEndApplication {
 
   public static void main(String[] args) {
-    System.out.println(1111);
     SpringApplication.run(TalksoBackEndApplication.class, args);
   }
 }
