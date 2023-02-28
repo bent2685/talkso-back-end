22fa63823a550d2978a5bfd1eff83f8961d508fe
zhang-refuses-to-accept <1432144374@qq,com>
2023-02-28 19:23:37 +0800

    cnmd

--- src/main/java/com/tzvtc/talksobackend/TalksoBackEndApplication.java
+++ src/main/java/com/tzvtc/talksobackend/TalksoBackEndApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TalksoBackEndApplication {
 
   public static void main(String[] args) {
-    System.out.println("HEllo WOrld");
+    System.out.println("Cnmd");
     SpringApplication.run(TalksoBackEndApplication.class, args);
   }
 
